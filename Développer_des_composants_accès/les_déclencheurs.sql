d61a7e2f3ea05d9ddb5d6a64bbdf0f2d2f28c2cc
LeoJab <leojabelin@laposte.net>
2023-11-07 16:34:38 +0100

    les déclencheurs phase 1

---
+++ Développer_des_composants_accès/les_déclencheurs.sql
@@ -0,0 +1,33 @@
+--Phase 1
+
+--1. modif_reservation : interdire la modification des réservations (on autorise l'ajout et la suppression).
+DELIMITER //
+CREATE TRIGGER modif_reservation BEFORE UPDATE ON reservation 
+for each row
+BEGIN
+    SIGNAL SQLSTATE '40000' SET MESSAGE_TEXT = 'Impossible de modifier une reservation !';
+END; //
+
+DELIMITER //
+CREATE TRIGGER insert_reservation BEFORE INSERT ON reservation
+for each row
+BEGIN
+    DECLARE cha_res_id INT;
+    DECLARE hot_id INT;
+    DECLARE nbr_res_hot INT;
+    
+    --Numéro de chambre de la reservation
+    SET cha_res_id = new.res_cha_id;
+    
+    --Recupération de l'id de l'hotel de la reservation
+    SELECT hot_id INTO hot_id FROM hotel 
+    INNER JOIN chambre ON cha_hot_id = hot_id
+    WHERE cha_id = cha_res_id;
+    
+
+    --SET nbr_res_hot = SELECT 
+END; //
+
+SELECT cha_id, res_cha_id FROM chambre
+INNER JOIN reservation ON res_cha_id = cha_id
+GROUP BY cha_id
