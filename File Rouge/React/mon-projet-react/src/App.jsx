f80193a3af18ea758f7ffd353ba1eacdacf7a5dc
LeoJab <leojabelin@laposte.net>
2024-02-22 16:26:08 +0100

    React

--- File Rouge/React/mon-projet-react/src/App.jsx
+++ File Rouge/React/mon-projet-react/src/App.jsx
@@ -1,5 +1,7 @@
 import Exo1 from "./Exo1";
 import Exo2 from "./Exo2";
+import Exo3 from "./Exo3";
+import Api from "./Api";
 
 function App() {
   return (
@@ -7,8 +9,17 @@ function App() {
     <div>
       <Exo1 />
     </div>
+    
     <div>
       <Exo2 />
+    </div>
+
+    <div>
+      <Exo3 />
+    </div>
+
+    <div>
+      <Api />
     </div>
   </>
   );
