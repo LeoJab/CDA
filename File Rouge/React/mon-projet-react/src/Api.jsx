b361f33df35d9b37c4840c5158eec5a7ec337d81
LeoJab <leojabelin@laposte.net>
2024-02-27 16:43:54 +0100

    react

--- File Rouge/React/mon-projet-react/src/Api.jsx
+++ File Rouge/React/mon-projet-react/src/Api.jsx
@@ -1,15 +1,46 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Api() {
-    axios.get('/user?ID=12345')
-    .then(function (response) {
-        // handle success
-        console.log(response);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    });
+    
+    const [nom, setNom] = useState('');
+
+    const handleRecherche = (event) => {
+        setNom(event.target.value);
+    }
+
+    const [liste, setListe] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://api.themoviedb.org/3/search/movie?api_key=f33cd318f5135dba306176c13104506a&query=${nom}`,
+            {
+                headers: { "Accept": "application/json" }
+            }
+        ).then( 
+            (response) => {
+                setListe(response.data);
+            }
+        )
+    }, [])
+
+    return (
+
+            <div>
+                <h2>API The Movie DB</h2>
+                <input type="text" placeholder="Entre un nom de film" value={nom} />
+                <button onClick={handleRecherche}>Rechercher</button>
+                {
+                    liste.map( (element, index) =>
+                        (
+                            <div key={index}>
+                                {element.title}
+                            </div>
+                        )
+                    )
+                }
+            </div>
+
+    )
 }
 
 export default Api;
